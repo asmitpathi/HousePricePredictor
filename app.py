d1f7fc6876d12504123d6d7f46d24aeacace5220
Asmit Pathi <asmitpathi27@gmail.com>
2026-06-06 17:03:08 +0530

    update mlmodel, homehtml and app

--- app.py
+++ app.py
@@ -10,6 +10,12 @@ regmodel= pickle.load(open('regmodel.pkl', 'rb'))
 #load scaler
 scaler= pickle.load(open('scaler.pkl', 'rb'))
 
+feature_names = [
+    'CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM',
+    'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO',
+    'B', 'LSTAT'
+]
+
 #home route
 @app.route('/')
 def home():
@@ -20,11 +26,33 @@ def home():
 def predict_api():
     data= request.json['data']   #capture the input data which is in json format
     print(data)
-    print(np.array(list(data.values())).reshape(1,-1))
-    new_data= scaler.transform(np.array(list(data.values())).reshape(1,-1))
-    output= regmodel.predict(new_data)
+    input_df = pd.DataFrame(
+        [list(data.values())],
+        columns=feature_names
+    )
+    scaled_data = scaler.transform(input_df)
+    output= regmodel.predict(scaled_data)
     print(output[0])
     return jsonify(output[0])
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    if len(data) != 13:
+        return render_template(
+            "home.html",
+            prediction_text="Please enter all 13 feature values."
+        )
+    input_df = pd.DataFrame(
+        [data],
+        columns=feature_names
+    )
+    scaled_data = scaler.transform(input_df)
+    output = regmodel.predict(scaled_data)[0]
+    return render_template(
+        "home.html",
+        prediction_text=f"The house price prediction is {output}"
+    )
 
 if __name__=='__main__':
     app.run(debug=True)
